2fd1a0aa686ff10174b034ceb2de1f31f288f065
Shivani <shivani.204286@gmail.com>
2023-07-15 23:54:30 -0700

    My first commit

---
+++ Shivani.js
@@ -0,0 +1,176 @@
+
+
+// 12-07-2023
+
+// function great(){
+
+// alert("Hello World!")
+
+
+// }
+
+
+
+
+// function ratio(){
+
+//    var input = document.getElementById("First name").value;
+//    document.getElementById("Text").innerHTML=input;
+
+
+//    }
+
+
+
+
+// 13-07-2023
+
+// document.getElementById("demo").onmouseover = function() {mouseOver()};
+
+// function mouseOver() {
+
+//    document.getElementById("demo").style.color = "Purple";
+
+//    }
+
+// document.getElementById("demo").onmouseout = function() {mouseOut()};
+// function mouseOut() {
+// document.getElementById("demo").style.color = "Red";
+// }
+
+
+
+
+// document.getElementById("Firstname").onchange = function() {myFunction()};
+// function myFunction() {
+//    var x = document.getElementById("Firstname");
+//    x.value = x.value.toUpperCase();
+//    }
+ 
+
+
+
+//    function checkyourCookies() {
+//     var text = "";
+//     if (navigator.cookieEnabled == true) {
+
+//     text = "your web page Cookies are active.";
+
+//     } else {
+
+//     text = "your web page Cookies are not active.";
+
+//     }
+//     document.getElementById("OnloadTest").innerHTML = text;
+
+//     }
+
+
+ 
+
+// document.getElementById("fname").addEventListener("blur", myFunction);
+// function myFunction() {
+// alert("your Input element lost focus.");
+// }
+
+
+// function changesize(){
+
+
+// document.getElementById("img").style.width="500px";
+
+
+// }
+
+
+
+// function changesize2(){
+
+
+//     document.getElementById("img").style.width="250px";
+    
+    
+//     }
+
+
+
+//  function changeImage(){
+
+
+//  document.getElementById("img").src="https://shorturl.at/flIJN";
+        
+        
+//  }
+
+//  function changeImage2(){
+
+//     document.getElementById("img").src="https://shorturl.at/flIJN";
+
+//  }
+
+
+
+// 15-07-2023
+// var num=0;
+// document.getElementById("number").innerHTML=num;
+
+
+// function inc(){
+// num++
+// document.getElementById("number").innerHTML=num;
+
+// }
+
+// function dec(){
+// num--
+// document.getElementById("number").innerHTML=num;
+
+
+// }
+
+
+// var b=0;
+// document.getElementById("element").innerHTML=b;
+
+// function increment(){
+// b ++
+// document.getElementById("element").innerHTML=b;
+
+// }
+
+// function dicrement(){
+// b --
+// document.getElementById("element").innerHTML=b;
+
+
+// }
+
+// function reset(){
+//     b=0
+   
+//     document.getElementById("element").innerHTML=b;
+
+// }
+
+
+
+
+// for table
+
+// var a = prompt("pls enter the table")
+
+
+// for(i=1; i<=10; i++){
+
+// document.write(i*a + "<br>")
+
+// }
+
+// or
+
+var a = prompt("pls enter the table")
+for(i=1; i<=10; i++)
+console.log(a*i)
+document.write()
+
+
